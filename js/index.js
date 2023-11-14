1730963c4859cd59d59b54010e83d02ab94d9eb0
Jose Luis Izquierdo Hernandez <110314046+jocl0110@users.noreply.github.com>
2023-11-14 16:36:55 -0600

    Insert script tag and JS file, fix some errors from the last assignment

---
+++ js/index.js
@@ -0,0 +1,21 @@
+const today = new Date();
+const thisYear = today.getFullYear();
+const footer = document.querySelector('footer');
+const copyright = document.createElement('p');
+
+const skillsSection = document.getElementById('skills');
+const skills = ['HTML5', 'CSS', 'JavaScript', 'Teamwork', 'Problem Solving', 'Algorithms' ];
+const skillsList = skillsSection.querySelector('ul');
+
+
+
+copyright.innerHTML = `Jose Izquierdo ${thisYear}`;
+footer.appendChild(copyright);
+
+for(let i = 0; i < skills.length; i++){
+    let skill = document.createElement('li');
+    skill.textContent = skills[i];
+    skillsList.appendChild(skill);
+}
+
+console.log(thisYear);
